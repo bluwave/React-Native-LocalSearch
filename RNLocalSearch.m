6fc09ffde774df4d802a5b5d20f823ae6a315322
Garrett Richards <garrettrichards@gmail.com>
2017-09-13 12:51:33 -0600

    change returned address keys, account for street2 and name

--- RNLocalSearch.m
+++ RNLocalSearch.m
@@ -20,13 +20,18 @@ RCT_EXPORT_MODULE()
     for (MKMapItem *mapItem in localSearchResponse.mapItems) {
         NSMutableDictionary *formedLocation = [[NSMutableDictionary alloc] init];
         NSMutableDictionary *pm = [[NSMutableDictionary alloc] init];
-        NSLog(@"address Dict: %@", mapItem.placemark.addressDictionary);
-//        [pm setValue:mapItem.placemark.thoroughfare forKey:@"thoroughfare"];
+        //        NSLog(@"address Dict: %@", mapItem.placemark.addressDictionary);
+        
         [pm setValue:[NSString stringWithFormat:@"%@ %@",mapItem.placemark.subThoroughfare, mapItem.placemark.thoroughfare]  forKey:@"street"];
+        NSString * street2 = [self parseStreet2:mapItem.placemark];
+        if([street2 length] > 0) {
+            [pm setValue:street2 forKey:@"street2"];
+        }
         [pm setValue:mapItem.placemark.locality forKey:@"city"];
         [pm setValue:mapItem.placemark.administrativeArea forKey:@"state"];
         [pm setValue:mapItem.placemark.postalCode forKey:@"zip"];
         [pm setValue:mapItem.placemark.ISOcountryCode forKey:@"country"];
+
         [formedLocation setValue:pm forKey:@"placemark"];
         [formedLocation setValue:mapItem.name forKey:@"name"];
         [formedLocation setValue:mapItem.placemark.title forKey:@"title"];
@@ -38,6 +43,41 @@ RCT_EXPORT_MODULE()
     }
     
     return [RCTResponse copy];
+}
+
+- (NSString*) parseStreet2: (MKPlacemark*) placemark {
+    NSArray * lines = placemark.addressDictionary[@"FormattedAddressLines"];
+    if(lines.count > 3 && placemark.thoroughfare) {
+        BOOL hasLocationName = ![lines.firstObject containsString:placemark.thoroughfare];
+        NSUInteger count = lines.count;
+        if(count == 5 && hasLocationName) {
+            return [lines objectAtIndex:2];
+        }
+        else if(count == 4) {
+            return [lines objectAtIndex:1];
+        }
+    }
+    return nil;
+    //    0) apple
+    //    1) 232 madison
+    //    2) #123
+    //    3) NYC NY 10016
+    //    4) US
+    //
+    //    0) apple
+    //    1) 232 madison
+    //    2) NYC NY 10016
+    //    3) US
+    //
+    //    0) 232 madison
+    //    1) #123
+    //    2) NYC NY 10016
+    //    3) US
+    //
+    //    0) 232 madison
+    //    1) NYC NY 10016
+    //    2) US
+    //
 }
 
 #pragma mark -
